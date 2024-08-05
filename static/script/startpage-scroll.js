3df33e436bcd43956b72d7cef99b72dc41f297d7
Johnson / Kingston <post@ivanweiss.net>
2024-08-05 11:51:00 +0200

    Update 5.8. v2

---
+++ static/script/startpage-scroll.js
@@ -0,0 +1,19 @@
+                var positions = [];
+                var activeSection;
+                var activeFilter1;
+
+                $('main').on( "scroll", function() {    
+                    $( "section" ).each(function( index ) {
+                        positions[index] = $('body').scrollTop() - $( this ).position().top;
+                        if(positions[index] > 0){
+                            activeSection = index;
+                            activeFilter1 = ((100 - (positions[index] / $('section').first().height() *100)) / 10)+1;
+                            activeFilter2 = ((100 - (positions[index] / $('section').first().height() *100)) / 10)+1;
+                            activeFilter3 = ((100 - (positions[index] / $('section').first().height() *100)) / 10)+1;
+                        }
+                    });
+
+                    $('#video'+(activeSection+2)).css('display','block');                    
+                    $('#video'+(activeSection+2)).css('filter','brightness('+activeFilter1+')');
+
+                });
